b5da4f14ec60b983804c533299fd3e7539a60605
Unknown <kajgore@vp.pl>
2020-09-03 21:31:57 +0200

    Fulfill db

--- src/psql/1createTables.sql
+++ src/psql/1createTables.sql
@@ -1,134 +1,133 @@
 CREATE TABLE public.login
 (
-    id bigserial NOT NULL,
-    email text NOT NULL,
-    password varchar(1024) NOT NULL,
-    creation_date date NOT NULL,
-    status varchar(16) NOT NULL,
+    id            bigserial     NOT NULL,
+    email         text          NOT NULL,
+    password      varchar(1024) NOT NULL,
+    creation_date date          NOT NULL,
+    status        varchar(16)   NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
 );
 
 CREATE TABLE public.student
 (
-    id bigserial NOT NULL,
-    id_login bigserial NOT NULL,
-    firstname varchar(128) NOT NULL,
-    lastname varchar(128) NOT NULL,
-    phone_number varchar(16) NOT NULL,
+    id           bigserial    NOT NULL,
+    id_login     bigserial    NOT NULL,
+    firstname    varchar(128) NOT NULL,
+    lastname     varchar(128) NOT NULL,
+    phone_number varchar(16)  NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY(id_login) REFERENCES public.login(id)
+    FOREIGN KEY (id_login) REFERENCES public.login (id)
 );
 
 CREATE TABLE public.instructor
 (
-    id bigserial NOT NULL,
-    id_login bigserial NOT NULL,
-    firstname varchar (128) NOT NULL,
-    lastname varchar (128) NOT NULL,
-    employment_date date NOT NULL,
-    dismissal_date date,
-    phone_number varchar(16) NOT NULL,
+    id              bigserial    NOT NULL,
+    id_login        bigserial    NOT NULL,
+    firstname       varchar(128) NOT NULL,
+    lastname        varchar(128) NOT NULL,
+    employment_date date         NOT NULL,
+    dismissal_date  date,
+    phone_number    varchar(16)  NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_login) REFERENCES public.login(id)
+    FOREIGN KEY (id_login) REFERENCES public.login (id)
 );
 
 CREATE TABLE public.admin
 (
-    id bigserial NOT NULL,
-    id_login bigserial NOT NULL,
-    firstname varchar (128) NOT NULL,
-    lastname varchar (128) NOT NULL,
-    phone_number varchar (16) NOT NULL,
+    id           bigserial    NOT NULL,
+    id_login     bigserial    NOT NULL,
+    firstname    varchar(128) NOT NULL,
+    lastname     varchar(128) NOT NULL,
+    phone_number varchar(16)  NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_login) REFERENCES public.login(id)
+    FOREIGN KEY (id_login) REFERENCES public.login (id)
 );
 
 CREATE TABLE public.category
 (
-    id bigserial NOT NULL,
-    category_name text NOT NULL,
-    category_type text NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (category_name)
-);
-
-CREATE TABLE public.course
-(
-    id bigserial NOT NULL,
-    id_student bigserial NOT NULL,
-    id_instructor bigserial NOT NULL,
-    id_category bigserial NOT NULL,
-    hours_minimum int NOT NULL,
-    hours_remaining int NOT NULL,
-    hours_done int NOT NULL,
-    status varchar(16) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_student) REFERENCES public.login(id),
-    FOREIGN KEY (id_instructor) REFERENCES public.login(id),
-    FOREIGN KEY (id_category) REFERENCES public.category(id)
+    id            bigserial NOT NULL,
+    category_name text      NOT NULL,
+    category_type text      NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE public.instructor_category
 (
     id_instructor bigserial NOT NULL,
-    id_category bigserial NOT NULL,
+    id_category   bigserial NOT NULL,
 
     PRIMARY KEY (id_instructor, id_category),
-    FOREIGN KEY (id_instructor) REFERENCES public.instructor(id),
-    FOREIGN KEY (id_category) REFERENCES public.category(id)
+    FOREIGN KEY (id_instructor) REFERENCES public.instructor (id),
+    FOREIGN KEY (id_category) REFERENCES public.category (id)
 );
 
 CREATE TABLE public.vehicle
 (
-    id bigserial NOT NULL,
-    mark varchar (128) NOT NULL,
-    model varchar (128) NOT NULL,
-    id_category bigserial NOT NULL,
-    plate_number varchar (128) NOT NULL,
-    vin varchar (128) NOT NULL,
+    id           bigserial    NOT NULL,
+    mark         varchar(128) NOT NULL,
+    model        varchar(128) NOT NULL,
+    id_category  bigserial    NOT NULL,
+    plate_number varchar(128) NOT NULL,
+    vin          varchar(128) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_category) REFERENCES public.category(id)
+    FOREIGN KEY (id_category) REFERENCES public.category (id)
 );
 
 CREATE TABLE public.instructor_vehicle
 (
-    id bigserial NOT NULL,
+    id            bigserial NOT NULL,
     id_instructor bigserial NOT NULL,
-    id_vehicle bigserial NOT NULL,
+    id_vehicle    bigserial NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_instructor) REFERENCES public.instructor(id),
-    FOREIGN KEY (id_vehicle) REFERENCES public.vehicle(id)
+    FOREIGN KEY (id_instructor) REFERENCES public.instructor (id),
+    FOREIGN KEY (id_vehicle) REFERENCES public.vehicle (id)
+);
+
+CREATE TABLE public.course
+(
+    id              bigserial   NOT NULL,
+    id_student      bigserial   NOT NULL,
+    id_instructor   bigserial   NOT NULL,
+    id_category     bigserial   NOT NULL,
+    hours_minimum   int         NOT NULL,
+    hours_remaining int         NOT NULL,
+    hours_done      int         NOT NULL,
+    status          varchar(16) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_student) REFERENCES public.login (id),
+    FOREIGN KEY (id_instructor) REFERENCES public.login (id),
+    FOREIGN KEY (id_category) REFERENCES public.category (id)
 );
 
 CREATE TABLE public.instructor_rating
 (
-    id bigserial NOT NULL,
-    id_student bigserial NOT NULL,
-    id_instructor bigserial NOT NULL,
-    grade int NOT NULL,
-    comment varchar (128) NOT NULL,
+    id            bigserial    NOT NULL,
+    id_student    bigserial    NOT NULL,
+    id_instructor bigserial    NOT NULL,
+    grade         int          NOT NULL,
+    comment       varchar(128) NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_student) REFERENCES public.student(id),
-    FOREIGN KEY (id_instructor) REFERENCES public.instructor(id)
+    FOREIGN KEY (id_student) REFERENCES public.student (id),
+    FOREIGN KEY (id_instructor) REFERENCES public.instructor (id)
 );
 
 CREATE TABLE public.event
 (
-    id bigserial NOT NULL,
-    id_student bigserial NOT NULL,
+    id            bigserial NOT NULL,
+    id_student    bigserial NOT NULL,
     id_instructor bigserial NOT NULL,
-    id_vehicle bigserial,
-    id_course bigserial NOT NULL,
-    duration int NOT NULL,
-    comment varchar(256),
+    id_vehicle    bigserial,
+    id_course     bigserial NOT NULL,
+    duration      int       NOT NULL,
+    comment       varchar(256),
     PRIMARY KEY (id),
-    FOREIGN KEY (id_student) REFERENCES public.student(id),
-    FOREIGN KEY (id_instructor) REFERENCES public.instructor(id),
-    FOREIGN KEY (id_vehicle) REFERENCES public.vehicle(id),
-    FOREIGN KEY (id_course) REFERENCES public.course(id)
+    FOREIGN KEY (id_student) REFERENCES public.student (id),
+    FOREIGN KEY (id_instructor) REFERENCES public.instructor (id),
+    FOREIGN KEY (id_vehicle) REFERENCES public.vehicle (id),
+    FOREIGN KEY (id_course) REFERENCES public.course (id)
 );
 
 
--- src/psql/2fulfilleTables.sql
+++ src/psql/2fulfilleTables.sql
@@ -16,12 +16,12 @@ INSERT INTO instructor(id_login, firstname, lastname, employment_date, phone_num
 values (4, 'Monika', 'Słoneczna', '2016-01-01', '554857689'),
        (5, 'Paweł', 'Andrut', '2019-03-01', '684584889');
 
-INSERT INTO admin(id_login, firstname, lastname, employment_date, phone_number)
-values (6, 'Tadeusz', 'Mysz', '2012-01-01', '556445778');
+INSERT INTO admin(id_login, firstname, lastname, phone_number)
+values (6, 'Tadeusz', 'Mysz', '556445778');
 
 INSERT INTO category(category_name, category_type)
 values ('AM', 'Teoria'),
-       ('A!', 'Teoria'),
+       ('A1', 'Teoria'),
        ('A2', 'Teoria'),
        ('A', 'Teoria'),
        ('B1', 'Teoria'),
@@ -33,8 +33,89 @@ values ('AM', 'Teoria'),
        ('C1+E', 'Teoria'),
        ('D', 'Teoria'),
        ('D+E', 'Teoria'),
-       ('C1+E', 'Teoria'),
        ('D1+E', 'Teoria'),
-       ('T', 'Teoria')
+       ('T', 'Teoria'),
+       ('Kwalifikacja wstępna', 'Teoria'),
+       ('AM', 'Praktyka'),
+       ('A1', 'Praktyka'),
+       ('A2', 'Praktyka'),
+       ('A', 'Praktyka'),
+       ('B1', 'Praktyka'),
+       ('B', 'Praktyka'),
+       ('B+E', 'Praktyka'),
+       ('C', 'Praktyka'),
+       ('C+E', 'Praktyka'),
+       ('C1', 'Praktyka'),
+       ('C1+E', 'Praktyka'),
+       ('D', 'Praktyka'),
+       ('D+E', 'Praktyka'),
+       ('C1+E', 'Praktyka'),
+       ('T', 'Praktyka');
 
-       --Jeszcze kwalifikacje wstepne
+INSERT INTO vehicle(mark, model, id_category, plate_number, vin)
+values ('Toyota', 'Yaris', 22, 'WZ351AW', 'JS1811116486A'),
+        ('Mercedes', 'Tir', 26, 'WB223AQ', 'JS12313131232'),
+        ('Renault', 'Autokar', 29, 'WWZ233', 'JS13123123123');
+
+INSERT INTO instructor_category(id_instructor, id_category)
+values (1, 1),
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (1, 5),
+        (1, 6),
+        (1, 7),
+        (1, 8),
+        (1, 9),
+        (1, 10),
+        (1, 11),
+        (1, 12),
+        (1, 13),
+        (1, 14),
+        (1, 15),
+        (1, 16),
+        (1, 17),
+        (1, 18),
+        (1, 19),
+        (1, 20),
+        (1, 21),
+        (1, 22),
+        (1, 23),
+        (2, 21),
+        (2, 22),
+        (2, 23),
+        (2, 24),
+        (2, 25),
+        (2, 26),
+        (2, 27),
+        (2, 28),
+        (2, 29),
+        (2, 30),
+        (2, 31);
+
+insert into instructor_vehicle(id_instructor, id_vehicle)
+values (1, 1),
+        (2, 1),
+        (2, 2),
+        (2, 3);
+
+insert into instructor_rating(id_student, id_instructor, grade, comment)
+values (1, 1, 5, 'OK');
+
+insert into course(id_student, id_instructor, id_category, hours_minimum, hours_remaining, hours_done, status)
+values (1, 1, 6, 30, 30, 0, 'IN_PROGRESS'),
+        values (1, 1, 22, 30, 0, 'WAITING'),
+        values (2, 1, 6, 30, 0, 30, 'FINISHED'),
+        values (1, 2, 22, 30, 8, 28, 'IN_PROGRESS'),
+        values (3, 2, )
+
+
+
+
+
+
+
+
+-- INSERT INTO course(id_student, id_instructor, id_category, hours_minimum, hours_remaining, hours_done, status)
+-- values (1, )
+
