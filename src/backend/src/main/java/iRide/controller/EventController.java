d5469782df6e541bc568a86d03b45d864f74ef88
Unknown <kajgore@vp.pl>
2020-10-26 22:49:41 +0100

    Moved compose to root

---
+++ src/backend/src/main/java/iRide/controller/EventController.java
@@ -0,0 +1,28 @@
+package iRide.controller;
+
+import iRide.service.Event.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+    private EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService){
+        this.eventService = eventService;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Integer> deleteById(@PathVariable int id){
+        this.eventService.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+}
---
+++ src/backend/src/main/java/iRide/service/Event/EventService.java
@@ -0,0 +1,22 @@
+package iRide.service.Event;
+
+import iRide.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    public EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+
+    public int deleteById(int id){
+        eventRepository.deleteById(id);
+        return id;
+    }
+
+}
